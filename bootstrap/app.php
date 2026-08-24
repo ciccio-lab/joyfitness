9f15c5fc085b32e28224aa556f0a973a93982737
ciccio-lab <francescomartucci2007@gmail.com>
2026-08-24 18:51:46 +0200

    Register coach.auth middleware alias and fix route caching

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\CoachAuthMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,8 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Questa riga dice a Laravel di fidarsi del reverse proxy HTTPS di Railway
         $middleware->trustProxies(at: '*');
+        
+        // Assegna l'alias 'coach.auth' al middleware reale
+        $middleware->alias([
+            'coach.auth' => CoachAuthMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
